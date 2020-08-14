3819467385c1910dfc24af140cf2a04149d4b881
Saltflsh <598155869@qq.com>
2020-08-15 04:30:38 +0800

    add test unit

---
+++ tests/fetch.test.js
@@ -0,0 +1,15 @@
+var fetcher = require('../fetcher')
+var expect = require('chai').expect
+
+require("dotenv").config();
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+
+it('Fetch contributors data', () => {
+    // test Promise in chai.expect
+    return fetcher.getContributorsList('pingcap', 'tidb', GITHUB_TOKEN, 20, 1)
+        .then((res) => {
+            expect(res.status).to.equal(200);
+            expect(res.data).to.be.an('array')
+        })
+})
