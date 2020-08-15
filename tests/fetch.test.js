918fa29fe166feecaa532c4767fc69c1a8ea3ad5
Saltflsh <598155869@qq.com>
2020-08-15 19:28:24 +0800

    set test timeout

--- tests/fetch.test.js
+++ tests/fetch.test.js
@@ -5,11 +5,13 @@ require("dotenv").config();
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 
-it('Fetch contributors data', () => {
-    // test Promise in chai.expect
-    return fetcher.getContributorsList('pingcap', 'tidb', GITHUB_TOKEN, 20, 1)
-        .then((res) => {
-            expect(res.status).to.equal(200);
-            expect(res.data).to.be.an('array')
-        })
+describe('Fetcher', () => {
+    it('Fetch contributors data', () => {
+        // test Promise in chai.expect
+        return fetcher.getContributorsList('pingcap', 'tidb', GITHUB_TOKEN, 20, 1)
+            .then((res) => {
+                expect(res.status).to.equal(200);
+                expect(res.data).to.be.an('array')
+            })
+    }).timeout(5000)
 })
